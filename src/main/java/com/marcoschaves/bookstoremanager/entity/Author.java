da86f307721ad78796be98ef5536c73139d7a53d
Marcos Fernandes <marcosanf.c@gmail.com>
2020-09-08 15:32:32 -0400

    Developed project entities

--- src/main/java/com/marcoschaves/bookstoremanager/entity/Author.java
+++ src/main/java/com/marcoschaves/bookstoremanager/entity/Author.java
@@ -1,9 +1,19 @@
-package entity;
+package com.marcoschaves.bookstoremanager.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Author {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
