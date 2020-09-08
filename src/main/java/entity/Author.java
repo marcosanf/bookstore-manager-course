662b72208debaae4fff0998d634860629f280942
Marcos Fernandes <marcosanf.c@gmail.com>
2020-09-07 23:49:47 -0400

    mapping of project entities

---
+++ src/main/java/entity/Author.java
@@ -0,0 +1,16 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
